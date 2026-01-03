533f8925a5e47ab20fd4abfa4cc2d849430035f2
Niranjan <Niranjan1@gmail.com>
2026-01-03 19:31:19 +0530

    Added Nav component

---
+++ gd-prep/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>
+        <nav>
+            <div className="logo">
+                <h1>GD Prep</h1>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
