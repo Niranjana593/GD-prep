3ae5f470817ff167eab64736c576191a041bd2dc
Niranjan <Niranjan1@gmail.com>
2026-01-08 23:25:03 +0530

    Created the how its word section with 4 card of highlighting the usage of an application

--- gd-prep/src/components/Home.jsx
+++ gd-prep/src/components/Home.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='min-h-screen bg-black'>
-      <div className='intro border-1 min-h-127.5 text-white flex flex-col md:flex-row justify-center items-center  gap-10'>
+      <div className='intro  min-h-127.5 text-white flex flex-col md:flex-row justify-center items-center  gap-10'>
         <div className="tagline w-[50%]  roboto font-block ">
           <h1 className='text-[31px] indent-10'>Master Group Discussions with AI and Peers. Ace Your Interviews.</h1>
-          <h1 className='font-[400]'>Prepare the Group discussion with the AI and Peer for your Interview, the compitative prepration.</h1>
+          <h1 className='font-[400]'>Prepare the Group discussion with the AI and Peer for your Interview, the compititive prepration.</h1>
           <div className='flex gap-8 mt-8'>
             <button className='px-8 rounded-4xl bg-[#028857] cursor-pointer font-[700] py-3 border-2'>Practice With AI</button>
             <button className='px-8 rounded-4xl bg-[#028857] cursor-pointer font-[700] py-3 border-2'>Practice With Peers</button>
@@ -23,6 +23,36 @@ const Home = () => {
           </div>
         </div>
         <img width={350} className='max-w-[50%]  shadow-lg shadow-white' src="home.png" alt="" />
+      </div>
+      <div className='w-full h-[0.3px] border-1 border-gray-700'></div>
+      <div className="working min-h-90">
+          <div className='text-[31px] text-white roboto text-center'>How It Works</div>
+          <div className="cards w-full text-white flex justify-center items-center gap-10 mt-10">
+              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+                <div className="card1content flex flex-col gap-3">
+                <img className='h-[157px] p-[13px]' src="card1.png" alt="" />
+                <p className='roboto font-medium text-center '>Choose Mode (AI / Peers)</p>
+                </div>
+              </div>
+              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+                <div className="card1content flex flex-col gap-3">
+                <img className='h-[157px] p-[13px]' src="card2.png" alt="" />
+                <p className='roboto font-medium text-center '>Get Interview-Style Topic</p>
+                </div>
+              </div>
+              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+                <div className="card1content flex flex-col gap-3">
+                <img className='h-[157px] p-[13px]' src="card3.png" alt="" />
+                <p className='roboto font-medium text-center '>Discuss with Timer</p>
+                </div>
+              </div>
+              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+                <div className="card1content flex flex-col gap-3">
+                <img className='h-[157px] p-[13px]' src="card4.png" alt="" />
+                <p className='roboto font-medium text-center '>Get Instant Feedback & Score</p>
+                </div>
+              </div>
+          </div>
       </div>
     </div>
   )
