177fb48d373666c4db9912d52b86797abd3e35eb
Niranjan <Niranjan1@gmail.com>
2026-01-08 23:47:43 +0530

    Added discription to each card

--- gd-prep/src/components/Home.jsx
+++ gd-prep/src/components/Home.jsx
@@ -25,31 +25,35 @@ const Home = () => {
         <img width={350} className='max-w-[50%]  shadow-lg shadow-white' src="home.png" alt="" />
       </div>
       <div className='w-full h-[0.3px] border-1 border-gray-700'></div>
-      <div className="working min-h-90">
+      <div className="working min-h-100">
           <div className='text-[31px] text-white roboto text-center'>How It Works</div>
           <div className="cards w-full text-white flex justify-center items-center gap-10 mt-10">
-              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-65 w-50 border-1 border-white shadow-md shadow-white">
                 <div className="card1content flex flex-col gap-3">
                 <img className='h-[157px] p-[13px]' src="card1.png" alt="" />
                 <p className='roboto font-medium text-center '>Choose Mode (AI / Peers)</p>
+                <p className='text-gray-500 pl-3 text-sm'>Practice with AI or join live peer discussions</p>
                 </div>
               </div>
-              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+              <div className="hover:scale-110 duration-300 ease-in-out  card2 h-65 w-50 border-1 border-white shadow-md shadow-white">
                 <div className="card1content flex flex-col gap-3">
                 <img className='h-[157px] p-[13px]' src="card2.png" alt="" />
                 <p className='roboto font-medium text-center '>Get Interview-Style Topic</p>
+                <p className='text-gray-500 pl-3 text-sm'>Real GD topics used in placements</p>
                 </div>
               </div>
-              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+              <div className="hover:scale-110 duration-300 ease-in-out  card3 h-65 w-50 border-1 border-white shadow-md shadow-white">
                 <div className="card1content flex flex-col gap-3">
                 <img className='h-[157px] p-[13px]' src="card3.png" alt="" />
                 <p className='roboto font-medium text-center '>Discuss with Timer</p>
+                <p className= 'pl-3 text-gray-500 text-sm'>Simulates real interview pressure</p>
                 </div>
               </div>
-              <div className="hover:scale-110 duration-300 ease-in-out  card1 h-60 w-50 border-1 border-white shadow-md shadow-white">
+              <div className="hover:scale-110 duration-300 ease-in-out  card4 h-65 w-50 border-1 border-white shadow-md shadow-white">
                 <div className="card1content flex flex-col gap-3">
                 <img className='h-[157px] p-[13px]' src="card4.png" alt="" />
                 <p className='roboto font-medium text-center '>Get Instant Feedback & Score</p>
+                <p className='pl-3 text-gray-500 text-sm'>Clarity, confidence analysis</p>
                 </div>
               </div>
           </div>
